b50be3f26e199ae877322aee63457a9746d096c5
Cooladamm <Adam.ljung1@Outlook.com>
2024-04-01 20:04:23 +0200

    Logga för skapa möte

--- bulb/src/app/page.tsx
+++ bulb/src/app/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 export default function Home() {
   return (
     
-      <h1 className="text-5xl w-full justify-center flex items-center">Welcome to Bulb platform!</h1>
+      <h1 className="text-5xl text-black w-full justify-center flex items-center">Welcome to Bulb platform!</h1>
     
   );
 }
