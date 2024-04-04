e1613150c33bc87ba6fc798d2ca829c35c26c598
Cooladamm <Adam.ljung1@Outlook.com>
2024-04-04 18:32:47 +0200

    Lagt till ruta för section

---
+++ bulb/src/app/Components/section.tsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+export function addSection() {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+
+    const toggleMenu = () => {
+        setMenuOpen(!menuOpen);
+    };
+
+    return (
+        <div className="relative w-full h-full">
+            <div className="border-2 w-11/12 h-11/12 text-center border-dashed cursor-pointer" onClick={toggleMenu}>
+                <p className="text-3xl">----Lägg till avsnitt----</p>
+            </div>
+            {menuOpen && (
+                <div className="absolute right-0 mt-2 w-48 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5">
+                    <div className="py-1">
+                        <button className="block px-4 py-2 text-sm text-primaryText hover:bg-gray-100">
+                            Add Item 1
+                        </button>
+                        <button className="block px-4 py-2 text-sm text-primaryText hover:bg-gray-100">
+                            Add Item 2
+                        </button>
+                        <button className="block px-4 py-2 text-sm text-primaryText hover:bg-gray-100">
+                            Add Item 3
+                        </button>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+};
--- bulb/src/app/meetings/[id]/page.tsx
+++ bulb/src/app/meetings/[id]/page.tsx
@@ -1,6 +1,10 @@
+'use client'
+
 import { Button } from "@nextui-org/react";
 import { FormEvent } from "react";
 import Link from "next/link";
+import {addSection} from "app/components/section";
+
 
 export default function createPage() {
 
@@ -21,24 +25,22 @@ export default function createPage() {
 
                 <div className="bg-secondaryGrey h-5/6 w-1 content-center"></div>
 
-                <div className="flex flex-col text-primaryText gap-10">
+                <div className="flex flex-col text-primaryText gap-10 w-11/12">
                     <div className="flex flex-col gap-2 ">
                         <p className="text-primaryText text-5xl font-bold">Untitled meeting</p>
                         <p className="text-primaryText text-sm">Det lilla roliga mötet</p>
                     </div>
 
-                    <div className="flex flex-row justify-between">
+                    <div className="flex flex-row gap-5">
                         <Button className="bg-white border-2 border-edge w-4 h-6 m-2">File</Button>
                         <Button className="bg-white border-2 border-edge w-4 h-6 m-2">Edit</Button>
                         <Button className="bg-white border-2 border-edge w-4 h-6 m-2">Insert</Button>
                         <Button className="bg-white border-2 border-edge w-4 h-6 m-2">Format</Button>
                         <Button className="bg-white border-2 border-edge w-4 h-6 m-2">Help</Button>
                     </div>
-                    <div className="flex ">
-                        <form action="" className="flex flex-col gap-20">
-                            <textarea name="" id="" cols={30} rows={10} placeholder="Placeholder" className="textBoxLines p-5 rounded border-2 border-gray-200"></textarea>
-                            <Button className="bg-white border-2 border-edge w-4 h-6 m-2">Create</Button>
-                        </form>
+
+                    <div className="flex w-11/12">
+                        {addSection()}
                     </div>
                 </div>
             </div>
