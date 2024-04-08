100a6944e3accb34879eb958701a13cdfd36d12f
bjoed735 <bjoed735@student.liu.se>
2024-04-08 14:04:51 +0200

    some stuff

--- databas/src/middleware/authMiddleware.ts
+++ databas/src/middleware/authMiddleware.ts
@@ -1,18 +1,38 @@
 import { NextFunction, Request, Response } from 'express';
 import * as jwt from 'jsonwebtoken';
 import dotenv from "dotenv";
+import jwksClient, { JwksClient } from 'jwks-rsa';
+
 
 dotenv.config();
 
 const CLIENT_SECRET = process.env.GOOGLE_CLIENT_SECRET || 'wrong';
 console.log(CLIENT_SECRET);
 
+const client = jwksClient({
+  jwksUri: 'https://www.googleapis.com/oauth2/v3/certs'
+});
+
+function getKey(header, callback){
+  client.getSigningKey(header.kid, (err, key) => {
+    const signingKey = key?.getPublicKey()
+    callback(null, signingKey);
+  });
+}
+
 export default function verifyToken (token : string) {
   console.log(token)
   if (!token) {
     return false;
   }
-  const decoded = jwt.verify(token, CLIENT_SECRET, {algorithms:['RS256']});
+  const decoded = jwt.verify(token, getKey, { algorithms: ['RS256'] }, (err, decoded) => {
+    if (err) {
+      console.log('Token cannot be verified', err);
+      return;
+    }
+    console.log('Decoded JWT:', decoded);
+    // Proceed with the decoded token (decoded is the payload of the JWT)
+  });
   console.log(decoded);
 }
 //
