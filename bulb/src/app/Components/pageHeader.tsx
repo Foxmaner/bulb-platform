974574f09d62625dffe582c7dd49449ed543e784
Eskil Brännerud <eskbr129@student.liu.se>
2024-04-12 09:32:38 +0200

    fixed error in testing

--- bulb/src/app/Components/pageHeader.tsx
+++ bulb/src/app/Components/pageHeader.tsx
@@ -15,9 +15,9 @@ function capitalizeFirstLetter(string: string) {
 }
 
 export default function PageHeader({ userName }: { userName: string }) {
-    const pathName = usePathname().slice(1)
-    const router = useRouter();
-    return (
+  const pathName = usePathname().slice(1)
+  const router = useRouter();
+  return (
         <div className="bg-primaryGrey w-full flex-col justify-between">
             <div className="bg-white w-full">
                 <h2 className="text-black">Good afternoon, {userName}</h2>
@@ -40,32 +40,31 @@ export default function PageHeader({ userName }: { userName: string }) {
                 <Button className="bg-white border-2 border-edge text-primaryText w-5 h-7 m-2">01</Button>
                 <Button className="bg-white border-2 border-edge text-primaryText w-5 h-7 m-2">Filter</Button>
             </div>
-        </div>
         <div className="flex-shrink-0">
           <Image src="/placeholder.jpg" alt="" height={122} width={400}></Image>
         </div>
-      </div>
 
-      <div className="flex gap-1 bg-white w-full">
-        <Button
-          onClick={() => router.push("/meetings/create")}
-          className="bg-white border-2 border-edge text-primaryText w-5 h-7 m-2"
-        >
-          Skapa
-        </Button>
-        <Button className="bg-white border-2 border-edge text-primaryText w-5 h-7 m-2">
-          Företag
-        </Button>
-        <Button className="bg-white border-2 border-edge text-primaryText w-5 h-7 m-2">
-          Månad
-        </Button>
-        <Button className="bg-white border-2 border-edge text-primaryText w-5 h-7 m-2">
-          01
-        </Button>
-        <Button className="bg-white border-2 border-edge text-primaryText w-5 h-7 m-2">
-          Filter
-        </Button>
-      </div>
+
+    <div className="flex gap-1 bg-white w-full">
+      <Button
+        onClick={() => router.push("/meetings/create")}
+        className="bg-white border-2 border-edge text-primaryText w-5 h-7 m-2"
+      >
+        Skapa
+      </Button>
+      <Button className="bg-white border-2 border-edge text-primaryText w-5 h-7 m-2">
+        Företag
+      </Button>
+      <Button className="bg-white border-2 border-edge text-primaryText w-5 h-7 m-2">
+        Månad
+      </Button>
+      <Button className="bg-white border-2 border-edge text-primaryText w-5 h-7 m-2">
+        01
+      </Button>
+      <Button className="bg-white border-2 border-edge text-primaryText w-5 h-7 m-2">
+        Filter
+      </Button>
     </div>
+    </div >
   );
 }
