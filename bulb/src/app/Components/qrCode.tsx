f6332e7babbe6e9c1008fb84250a340a5dfc4e0c
foxmaner <eskil.brann@gmail.com>
2024-04-14 11:59:56 +0200

    worked on qrCode

--- bulb/src/app/Components/qrCode.tsx
+++ bulb/src/app/Components/qrCode.tsx
@@ -1,35 +1,54 @@
 'use client'
 import QRCode from "react-qr-code"
 
-import {Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure} from "@nextui-org/react";
+import { Modal, ModalContent, ModalHeader, ModalBody, ModalFooter, Button, useDisclosure } from "@nextui-org/react";
+
+import { Card, CardHeader, CardBody, CardFooter } from "@nextui-org/react";
 
 export default function Page({ qrData }: { qrData: string }) {
-    const {isOpen, onOpen, onOpenChange} = useDisclosure();
-    return (
-        
+  const { isOpen, onOpen, onOpenChange } = useDisclosure();
+  return (
+
+    <>
+
+
+      <Card className="absolute bottom-0 right-0 m-5" onPress={onOpen} isPressable={true}>
+        <CardBody>
+          <div className="flex items-center space-x-4">
+            <QRCode value={qrData} className="w-20 h-20" />
+            <div className="space-y-4 flex-col">
+              <p className="text-xl font-bold underline">Kod</p>
+              <div className="bg-white">
+                <p className="text-center text-2xl font-bold underline ">{qrData}</p>
+              </div>
+            </div>
+          </div>
+        </CardBody>
+      </Card>
+
+
+
+      <Modal isOpen={isOpen} onOpenChange={onOpenChange} className="absolute top-1/4 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
+        <ModalContent>
+          {(onClose) => (
             <>
-              <Button className="absolute bottom-0 right-0 m-5" onPress={onOpen}>Bjud in</Button>
-              <Modal isOpen={isOpen} onOpenChange={onOpenChange} className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-                <ModalContent>
-                  {(onClose) => (
-                    <>
-                      <ModalHeader className="flex flex-col gap-1">Modal Title</ModalHeader>
-                      <ModalBody>
-                        <QRCode value={qrData} />
-                      </ModalBody>
-                      <ModalFooter>
-                        <Button color="danger" variant="light" onPress={onClose}>
-                          Close
-                        </Button>
-                        <Button color="primary" onPress={onClose}>
-                          Action
-                        </Button>
-                      </ModalFooter>
-                    </>
-                  )}
-                </ModalContent>
-              </Modal>
+              <ModalBody className="bg-primaryGrey">
+                <div className="flex items-center space-x-4">
+                  <QRCode value={qrData} />
+                  <div className="space-y-4 flex-col">
+                    <p className="text-4xl font-bold underline">Kod</p>
+                    <div className="bg-white">
+                      <p className="text-center text-3xl font-bold underline ">{qrData}</p>
+                    </div>
+                  </div>
+                </div>
+              </ModalBody>
+
             </>
-          );
-    
+          )}
+        </ModalContent>
+      </Modal>
+    </>
+  );
+
 }
