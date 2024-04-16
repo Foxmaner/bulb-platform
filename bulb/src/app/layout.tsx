ee40d4328f0e961b3ba4d9f2c97d608c5795e2fa
Eskil Brännerud <eskbr129@student.liu.se>
2024-04-16 16:34:49 +0200

    added cypress test

--- bulb/src/app/layout.tsx
+++ bulb/src/app/layout.tsx
@@ -4,7 +4,6 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
-import { Inter } from "next/font/google";
 import Sidebar from "./components/Sidebar";
 
 const inter = Inter({ subsets: ["latin"] });
