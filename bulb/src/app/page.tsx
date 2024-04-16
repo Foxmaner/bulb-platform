c630be438747afda7c19925cf5987280a5566515
cecal690 <cecal690@student.liu.se>
2024-04-16 19:04:54 +0200

    färdig med att lägga in bilder

--- bulb/src/app/page.tsx
+++ bulb/src/app/page.tsx
@@ -4,14 +4,24 @@ import Image from "next/image";
 import Link from "next/link";
 import SignInButton from "./Components/SignInBtn";
 import loginImage from "../../public/login.svg";
+import loginImage2 from "../../public/login2.svg";
+import loginImage3 from "../../public/login3.svg";
+import loginImage4 from "../../public/login4.svg";
 
 export default function Home() {
   return (
     <div className="flex w-screen h-screen flex-shrink-0">
       {/* Vänstra halvan */}
-      <div className="bg-primary flex w-full md:w-1/2 place-items-center place-content-center">
+      <div className="bg-primary flex w-full md:w-1/2 place-items-center place-content-center relative">
+        {/* Upper left images */}
+        <div className="w-1/6 lg:w-1/7 z-10 absolute top-0 left-0">
+          <Image src={loginImage3} alt="Login Image" />
+          <Image src={loginImage4} alt="Login Image" />
+        </div>
+
         {/* Login-ruta */}
-        <div className="border-white border-2">
+        {/* TODO: göra rutan större för större skärmar */}
+        <div className="border-white border-2 z-20">
           <div className="bg-white border-primary border-4 flex flex-col">
             {/* East Sweden Medtech */}
             <div className="p-5">
@@ -33,21 +43,26 @@ export default function Home() {
             </Link>
           </div>
         </div>
+
+        {/* Lower right image */}
+        <div className="w-1/6 lg:w-1/7 z-10 absolute bottom-0 right-0">
+          <Image src={loginImage2} alt="Login Image" />
+        </div>
       </div>
 
       {/* Högra halvan */}
       <div className="bg-white w-0 md:w-1/2 flex flex-col flex-shrink-0 overflow-hidden relative">
         <div className="p-10 flex flex-row-reverse overflow-hidden">
           {/* Textruta */}
-          <div className="bg-primary p-1">
+          <div className="bg-primary p-1 z-20">
             <h1 className="text-white font-bold text-4xl lg:text-5xl border-white border-2 py-10 px-20">
               East <br /> Sweden <br /> MedTech
             </h1>
           </div>
         </div>
         {/* Lower right image */}
-        <div className="flex w-4/5 absolute bottom-0 right-0">
-          <Image src={loginImage} alt="Login Image"></Image>
+        <div className="flex w-4/5 z-10 absolute bottom-0 right-0 ">
+          <Image src={loginImage} alt="Login Image" />
         </div>
       </div>
     </div>
