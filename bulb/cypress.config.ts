7bfd491707af16554290c2c1afd66303b5e5052c
bjoed735 <bjoed735@student.liu.se>
2024-04-16 22:51:00 +0200

    se om det var problem med config filen

--- bulb/cypress.config.ts
+++ bulb/cypress.config.ts
@@ -1,10 +1,13 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress'
 
 export default defineConfig({
-  projectId: "mz1vne",
   e2e: {
-    setupNodeEvents(on, config) {
-      // implement node event listeners here
-    },
+    baseUrl: 'http://localhost:3000',
+    defaultCommandTimeout: 20000,
+    taskTimeout: 30000,
+    video: true,
+    screenshotsFolder: 'cypress/screenshots',
+    videosFolder: 'cypress/videos',
   },
-});
+})
+
