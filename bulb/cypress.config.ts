e4bf32b77771e8b0e4583ed77de69536e7d7d5a5
bjoed735 <bjoed735@student.liu.se>
2024-04-16 23:11:33 +0200

    råkade ta bort något viktigt men kanske funkar nu vem vet!

--- bulb/cypress.config.ts
+++ bulb/cypress.config.ts
@@ -1,13 +1,15 @@
 import { defineConfig } from 'cypress'
 
 export default defineConfig({
+  projectId: "mz1vne",
   e2e: {
     baseUrl: 'http://localhost:3000',
     defaultCommandTimeout: 20000,
     taskTimeout: 30000,
-    video: true,
-    screenshotsFolder: 'cypress/screenshots',
-    videosFolder: 'cypress/videos',
+
   },
 })
 
+//video: true,
+//screenshotsFolder: 'cypress/screenshots',
+//videosFolder: 'cypress/videos',
