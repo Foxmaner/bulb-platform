52c015625b45155cf43a66dad4a505e17e31f0fa
foxmaner <eskil.brann@gmail.com>
2024-04-18 11:28:31 +0200

    added so it loads in notes from data

--- frontend/src/app/components/stickerNote.tsx
+++ frontend/src/app/components/stickerNote.tsx
@@ -2,12 +2,45 @@
 import React from 'react';
 import Draggable from 'react-draggable';
 
+let sampleData = {
+    [0]: {
+        id: 0,
+        content: "This is a sticker note!",
+        position: {
+            x: 100,
+            y: 50
+        }
+    },
+    [1]: {
+        id: 1,
+        content: "This is another sticker note!",
+        position: {
+            x: 11,
+            y: 11
+        }
+    }
+}
+
+function generateStickerNotes(sampleData: any) {
+    let stickerNotes = [];
+    for (let i = 0; i < sampleData.length; i++) {
+        stickerNotes.push(
+            <Draggable defaultPosition={sampleData[i].position}>
+                <div className="bg-yellow-200 p-4 aspect-square shadow-xl cursor-grab active:cursor-grabbing">
+                    <p className="text-yellow-800">{sampleData[i].content}</p>
+                </div>
+            </Draggable>
+        );
+    }
+    return stickerNotes;
+}
+
 export default function StickerNote() {
     return (
-        <Draggable defaultPosition={{x: 0, y: 0}}>
-            <div className="bg-yellow-200 p-4 aspect-square shadow-xl cursor-grab active:cursor-grabbing">
-                <p className="text-yellow-800">This is a sticker note!</p>
-            </div>
-        </Draggable>
+        
+        <> 
+        {generateStickerNotes(Object.values(sampleData))}
+        </>
+        
     );
 };
