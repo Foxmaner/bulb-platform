31bf9e887299b9cc07929316fa09b3a83702dfcc
Mårten Saltin <marten@voxo.ai>
2024-04-19 19:58:35 +0200

    front page

--- bulb/src/app/page.tsx
+++ bulb/src/app/page.tsx
@@ -9,65 +9,58 @@ import loginImage3 from "../../public/login3.svg";
 import loginImage4 from "../../public/login4.svg";
 
 export default function Home() {
-  return (
-    <div className="flex w-screen h-screen flex-shrink-0">
-      {/* Vänstra halvan */}
-      <div className="bg-primary flex w-full md:w-1/2 place-items-center place-content-center relative">
-        {/* Upper left images */}
-        <div className="w-1/6 lg:w-1/7 z-10 absolute top-0 left-0">
-          <Image src={loginImage3} alt="Login Image" />
-          <Image src={loginImage4} alt="Login Image" />
-        </div>
+    return (
+		<div className="flex flex-col w-screen flex-shrink-0">
+			<div className="w-full h-[calc(100vh-1rem)] relative flex items-center justify-center flex-col">
+				<h1 className="text-white font-bold text-8xl z-10 text-cente p-4">BULB</h1>
+				<h2 className="text-thridGrey text-3xl z-10 text-center">Varje vårdsituation är en möjlighet till förändring.</h2>
+				<div className="flex flex-row items-center justify-center w-full z-10 m-4">
+					<Link href={"/loginPage"} className="m-4 bg-primaryLight text-white font-bold text-xl px-6 py-2 rounded-lg">
+						Börja Nu
+					</Link>
+					<Link href={"https://eastswedenmedtech.se/en/start-uk/"} className="m-4 bg-primaryLight text-white font-bold text-xl px-6 py-2 rounded-lg">
+						Om Oss
+					</Link>
+				</div>
 
-        {/* Behov-ruta */}
-        {/* TODO: göra rutan större för större skärmar */}
-        <div className="border-white border-2 z-20 flex flex-col">
-          <div className="bg-white border-primary border-4 flex flex-col">
-            {/* East Sweden Medtech */}
-            <div className="p-5">
-              <h1 className="text-primary font-bold text-base">
-                EAST <br /> SWEDEN <br /> MEDTECH
-              </h1>
-              <div className="bg-primary h-1 w-2/5"></div>
-            </div>
-            {/* Behov */}
-            {/* DET SER INTE BRA UT */}
-            <div className="p-10 self-center">
-              <h1 className="text-primary text-center text-2xl">
-                {/* Det ska egentligen en summa istället för 1400 */}
-                Just nu finns det <br /> 1400 <br /> behov inom vården
-              </h1>
-            </div>
-            <div className="bg-gray-300 h-1 mx-7"></div>
+				<div className="absolute top-0 left-0 h-full w-full bg-[url('/liu_university.png')] bg-cover"></div>
+				<div className="absolute top-0 left-0 h-full w-full bg-primary bg-opacity-50 z-1"></div>
 
-            <div className="text-center p-10">
-              <Link href="/loginPage" className="font-bold">Logga in </Link>
-              för att se behov
-            </div>
-          </div>
-        </div>
+				<h1 className="absolute top-[calc(5%)] left-[calc(5%)] text-white font-bold text-5xl">
+					EAST <br /> SWEDEN <br /> MEDTECH
+				</h1>
+				<Link href={"/loginPage"} className="absolute top-[calc(5%)] right-[calc(5%)] bg-primaryLight text-white font-bold text-xl px-6 py-2 rounded-lg">
+					Logga in
+				</Link>
 
-        {/* Lower right image */}
-        <div className="w-1/6 lg:w-1/7 z-10 absolute bottom-0 right-0">
-          <Image src={loginImage2} alt="Login Image" />
-        </div>
-      </div>
 
-      {/* Högra halvan */}
-      <div className="bg-white w-0 md:w-1/2 flex flex-col flex-shrink-0 overflow-hidden relative">
-        <div className="p-10 flex flex-row-reverse overflow-hidden">
-          {/* Textruta */}
-          <div className="bg-primary p-1 z-20">
-            <h1 className="text-white font-bold text-4xl lg:text-5xl border-white border-2 py-10 px-20">
-              East <br /> Sweden <br /> MedTech
-            </h1>
-          </div>
-        </div>
-        {/* Lower right image */}
-        <div className="flex w-4/5 z-10 absolute bottom-0 right-0 ">
-          <Image src={loginImage} alt="Login Image" />
-        </div>
-      </div>
-    </div>
-  );
+			</div>
+			<div className="h-[calc(60vh)] w-full bg-white p-16 relative">
+				<h1 className="font-bold text-5xl text-primary">Visste Du? Att Det Finns Över...</h1>
+				<div className="flex flex-row justify-evenly items-center h-4/5 mt-4">
+					<div className="flex flex-col items-center justify-center">
+						<h1 className="font-bold text-5xl text-primary m-2">1400</h1>
+						<h2 className="font-bold text-2xl text-primaryText">Alltal behov i vården</h2>
+					</div>
+					<div className="flex flex-col items-center justify-center">
+						<h1 className="font-bold text-5xl text-primary m-2">1400</h1>
+						<h2 className="font-bold text-2xl text-primaryText">Alltalet vi försöker lösa</h2>
+					</div>
+					<div className="flex flex-col items-center justify-center">
+						<h1 className="font-bold text-5xl text-primary m-2">1400</h1>
+						<h2 className="font-bold text-2xl text-primaryText">Antalet du kan fixa</h2>
+					</div>
+				</div>
+				<div className="absolute -top-1 right-0">
+
+				</div>
+			</div>
+			<div className="bg-primary h-[calc(90vh)] w-full flex flex-col items-center justify-center">
+				<h1 className="font-bold text-5xl text-white text-left w-full px-16">Demo</h1>
+				<h2 className="font-bold text-2xl text-primaryLight text-left w-full px-16">Skapa ditt nästa möte nu</h2>
+				<div className="h-3/5 w-3/5 bg-[url('/laptop.png')] bg-cover"></div>
+			</div>
+			
+		</div>
+	);
 }
--- bulb/tailwind.config.ts
+++ bulb/tailwind.config.ts
@@ -1,36 +1,37 @@
 import type { Config } from "tailwindcss";
 
 
-const {nextui} = require("@nextui-org/react");
+const { nextui } = require("@nextui-org/react");
 
 const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
-  ],
-  theme: {
-    extend: {
-      colors:{
-        'primary': '#512641',
-        'primaryGrey': '#F5F5F5', //Tidigare använd färg är bg-slate-100. Detta är den gråa färgen
-        'secondaryGrey':'#888888', 
-        'edge': "#D9D9D9", //Bordercolor
-        'titleText':'#333333',
-        'primaryText': "#888888",
-        
-      },
+    content: [
+        "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+        "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+        "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                primary: "#512641",
+                primaryLight: "#B0538D",
+                primaryGrey: "#F5F5F5", //Tidigare använd färg är bg-slate-100. Detta är den gråa färgen
+                secondaryGrey: "#888888",
+                thridGrey: "#D9D9D9",
+                edge: "#D9D9D9", //Bordercolor
+                titleText: "#333333",
+                primaryText: "#888888",
+            },
 
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-        "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+            backgroundImage: {
+                "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+                "gradient-conic":
+                    "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+            },
+        },
     },
-  },
-  darkMode: "class",
-  plugins: [nextui()],
+    darkMode: "class",
+    plugins: [nextui()],
 };
 
 
