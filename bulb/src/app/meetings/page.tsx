7ba583798d426d7b738fd48887cfd96900db169c
cecal690 <cecal690@student.liu.se>
2024-04-20 22:17:45 +0200

    knapparna som försvann är tillbaka

--- bulb/src/app/meetings/page.tsx
+++ bulb/src/app/meetings/page.tsx
@@ -3,14 +3,12 @@ import MeetingsView from "app/components/meetingList";
 
 export default function meetingPage() {
   return (
-    <div className="bg-primaryGrey w-full py-3 pr-6">
+    <div className="bg-primaryGrey w-full h-screen py-3 pr-6">
       <div className="flex flex-col rounded-2xl w-full h-full bg-white border-1 border-edge">
         <div className="rounded-t-2xl overflow-hidden">
           <PageHeader userName="Eskil" />
         </div>
-        <div className="h-full mt-4">
-          <MeetingsView />
-        </div>
+        <MeetingsView />
       </div>
     </div>
   );
