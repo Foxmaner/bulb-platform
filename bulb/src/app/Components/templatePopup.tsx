3f9ae48c2f1d40c8b7f9263f1aca7c4890f29852
cecal690 <cecal690@student.liu.se>
2024-04-20 23:58:48 +0200

    finns en (typ) fungerande popup men designen är inte klar

--- bulb/src/app/Components/templatePopup.tsx
+++ bulb/src/app/Components/templatePopup.tsx
@@ -1,8 +1,12 @@
 import TemplatesView from "./templateList";
 import { useState } from "react";
 import { Button } from "@nextui-org/react";
+import { usePathname, useRouter } from "next/navigation";
 
 export default function TemplatePopup() {
+  const router = useRouter();
+  const pathname = usePathname();
+
   const [isOpen, setItOpen] = useState(false);
   const openPopup = () => {
     setItOpen(true);
@@ -13,23 +17,42 @@ export default function TemplatePopup() {
 
   return (
     <div>
+      {/* Skapa-knappen som finns i headern */}
       <Button
-        onClick={openPopup}
+        onClick={
+          pathname.includes("/meetings")
+            ? openPopup
+            : () => router.push("/meetings/create") //ska ändras till templates/create när vi har det
+        }
         className="bg-white border-2 border-edge text-primaryText w-5 h-7 m-2"
       >
         {" "}
         Skapa
       </Button>
+
       {isOpen && (
         <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75 z-50">
           <div className="bg-white p-5 rounded-2xl">
+            {/* Rubrik */}
             <h2 className="text-xl font-bold mb-4">Mallar</h2>
+
+            {/* Lista på mallar */}
             <TemplatesView />
+
+            {/* Close-knapp */}
             <Button
               onClick={closePopup}
               className="mt-4 bg-blue-500 text-white py-2 px-4 rounded"
             >
               Close
+            </Button>
+
+            {/* Skapa utan mall-knapp */}
+            <Button
+              onClick={() => router.push("/meetings/create")}
+              className="bg-white border-2 border-edge text-primaryText font-bold rounded h-7 m-2"
+            >
+              Skapa utan mall
             </Button>
           </div>
         </div>
