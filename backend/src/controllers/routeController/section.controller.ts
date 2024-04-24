1e9759cc314d13451746a13a9db63043a3df5801
conqu301 <conqu301@liu.se>
2024-04-24 13:24:50 +0200

    Fixade sectionController

--- backend/src/controllers/routeController/section.controller.ts
+++ backend/src/controllers/routeController/section.controller.ts
@@ -5,28 +5,19 @@ import { MeetingModel } from '../../models';
 
 export default class SectionController {
     
-    //Fungerar ej. addSection krånglar
     static async create(req: Request, res: Response) {
-        const meetingId = req.params.meetingId
-
-        const meeting = await MeetingModel.findById(meetingId)
-
-        const resp = await meeting.addSection()
-
-        return res.status(resp.statusCode).json(resp.body)
+        const meetingId = req.params.meetingId;
+        const meeting = await MeetingModel.findById(meetingId);
+        const resp = await meeting.addSection();
+        return res.status(resp.statusCode).json(resp.body);
     }
 
     static async delete(req: Request, res: Response) {
-        const id = req.params.meetingId
-
-        //vet inte vad rätt syntax för att skaffa meetingId i denna call
-        const meetingId = req.params.meetingID 
-
-        const meeting = await MeetingModel.get(meetingId)
-
-        const resp = await meeting.removeSection(meetingId)
-
-        return res.status(resp.statusCode).json(resp.body)
+        const meetingID = req.params.meetingID;
+        const sectionID = req.params.sectionID;
+        const meeting = await MeetingModel.get(meetingID);
+        const resp = await meeting.removeSection(sectionID);
+        return res.status(resp.statusCode).json(resp.body);
     }
 
     static edit(req: Request, res: Response){
