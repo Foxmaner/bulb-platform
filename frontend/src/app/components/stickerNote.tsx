193b56d5e5680644d4c718e088013a2f813558c9
foxmaner <eskil.brann@gmail.com>
2024-05-05 12:14:15 +0200

    try to fix brainstorm

--- frontend/src/app/components/stickerNote.tsx
+++ frontend/src/app/components/stickerNote.tsx
@@ -1,5 +1,6 @@
 "use client"
-import React from 'react';
+import { stringify } from 'querystring';
+import React, { useEffect, useState } from 'react';
 import Draggable from 'react-draggable';
 
 let sampleData = {
@@ -7,27 +8,53 @@ let sampleData = {
         id: 0,
         content: "This is a sticker note!",
         position: {
-            x: 100,
-            y: 50
+            x: 0.5,
+            y: 0.1
         }
     },
     [1]: {
         id: 1,
         content: "This is another sticker note!",
         position: {
-            x: 11,
-            y: 11
+            x: 0.1,
+            y: 0.2
         }
     }
 }
 
+function onDragEvent(i: number) {
+    console.log((sampleData[i].position.x).toString());
+    
+}
+
+
 function generateStickerNotes(sampleData: any) {
+    const [windowSize, setWindowSize] = useState({ width: window.innerWidth, height: window.innerHeight });
+
+    useEffect(() => {
+        const handleResize = () => {
+          console.log('resizing');  
+          setWindowSize({ width: window.innerWidth, height: window.innerHeight });
+        };
+    
+        window.addEventListener('resize', handleResize);
+    
+        return () => {
+          window.removeEventListener('resize', handleResize);
+        };
+      }, []);
+
     let stickerNotes = [];
     for (let i = 0; i < sampleData.length; i++) {
+        let position = {
+            x: sampleData[i].position.x * windowSize.width,
+            y: sampleData[i].position.y * windowSize.height,
+          };
+
         stickerNotes.push(
-            <Draggable defaultPosition={sampleData[i].position}>
+            <Draggable defaultPosition={position} position={position} bounds="parent" onDrag={() => onDragEvent(DraggableData)}>
                 <div className="bg-yellow-200 p-4 aspect-square shadow-xl cursor-grab active:cursor-grabbing w-fit max-w-40 text-balance">
-                    <p className="text-yellow-800">{sampleData[i].content}</p>
+                    <p className="text-yellow-800 text-sm">{sampleData[i].content}</p>
                 </div>
             </Draggable>
         );
